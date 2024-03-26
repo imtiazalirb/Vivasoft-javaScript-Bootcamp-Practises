b8ec1f050b929b68a9387556502c59c7cc24701b
imtiazalirb <imtiazali4090@gmail.com>
2024-03-27 02:29:35 +0600

    feat: example 1

---
+++ Execution_context/example1/script.js
@@ -0,0 +1,8 @@
+let x =10; // 10 is assigned in x 
+
+function timesTen(a){ //timesTen function takes parameter a
+    return a * 10; // parameter is returned to the  
+}
+
+let y = timesTen(x);
+console.log(y);
