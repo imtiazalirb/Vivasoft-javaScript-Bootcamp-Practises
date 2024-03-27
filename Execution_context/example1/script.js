7558c740ace4c867c274ed94aad236280f19c910
imtiazalirb <imtiazali4090@gmail.com>
2024-03-28 03:11:58 +0600

    fix: comments addde for better understanding

--- Execution_context/example1/script.js
+++ Execution_context/example1/script.js
@@ -1,8 +1,9 @@
 let x =10; // 10 is assigned in x 
 
 function timesTen(a){ //timesTen function takes parameter a
-    return a * 10; // parameter is returned to the  
+    return a * 10; // parameter is returned to the line 7
 }
 
-let y = timesTen(x);
-console.log(y);
+let y = timesTen(x); // 1. timesTen function is called with parameter x (X has value of 10)
+                     // 2. timesTen(x) is replaced with returned value and assigned to y
+console.log(y); //value of y is printed. 
